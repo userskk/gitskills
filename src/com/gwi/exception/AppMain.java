b0a677318945a3ab3a5e3445b5469eab8e86f9c0
songkk <songkk@qq.com>
2017-06-30 16:04:24 +0800

    1

--- src/com/gwi/exception/AppMain.java
+++ src/com/gwi/exception/AppMain.java
@@ -2,11 +2,7 @@ package com.gwi.exception;
 
 public class AppMain {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		//1 2 3 4 5 2
-=======
-		//1 2 3 4 5 11
->>>>>>> branch 'master' of https://github.com/userskk/gitskills.git
+		//1
 
 	}
 }
